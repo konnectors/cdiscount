a3c7a183813002273c8f9b046c16d93f61c3f3fb
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2023-09-07 18:41:13 +0200

    feat: Let user login, detect login and save credentials

--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@ import Minilog from '@cozy/minilog'
 const log = Minilog('ContentScript')
 Minilog.enable('cdiscountCCC')
 
+const baseUrl = 'https://cdiscount.com'
+const loginFormUrl =
+  'https://order.cdiscount.com/Phone/Account/LoginLight.html?referrer='
 
-class TemplateContentScript extends ContentScript {
+class CdiscountContentScript extends ContentScript {
   onWorkerReady() {
     window.addEventListener('DOMContentLoaded', () => {
-      const button = document.querySelector('input[type=submit]')
-      if (button) {
-        button.addEventListener('click', () =>
-          this.bridge.emit('workerEvent', { event: 'loginSubmit' })
-        )
+      const loginForm = document.querySelector('#LoginForm')
+      if (loginForm) {
+        loginForm.addEventListener('submit', () => {
+          this.log('info', 'event submit detected')
+          const email = document.querySelector(
+            '#CustomerLogin_CustomerLoginFormData_Email'
+          )?.value
+          const password = document.querySelector(
+            '#CustomerLogin_CustomerLoginFormData_Password'
+          )?.value
+          this.bridge.emit('workerEvent', {
+            event: 'loginSubmit',
+            payload: { email, password }
+          })
+        })
       }
       const error = document.querySelector('.error')
       if (error) {
@@ -26,6 +39,15 @@ class TemplateContentScript extends ContentScript {
   onWorkerEvent({ event, payload }) {
     if (event === 'loginSubmit') {
       this.log('info', 'received loginSubmit, blocking user interactions')
+      const { email, password } = payload || {}
+      if (email && password) {
+        this.log('info', 'Couple email/password found')
+        this.log(
+          'info',
+          `{email,password} : ${JSON.stringify({ email, password })}`
+        )
+        this.store.userCredentials = { email, password }
+      }
       this.blockWorkerInteractions()
     } else if (event === 'loginError') {
       this.log(
@@ -39,10 +61,16 @@ class TemplateContentScript extends ContentScript {
   async ensureAuthenticated({ account }) {
     this.bridge.addEventListener('workerEvent', this.onWorkerEvent.bind(this))
     this.log('info', '🤖 ensureAuthenticated')
-    if (!account) {
-      await this.ensureNotAuthenticated()
+    // if (!account) {
+    //   await this.ensureNotAuthenticated()
+    // }
+    if (
+      !(await this.isElementInWorker(
+        '#CustomerLogin_CustomerLoginFormData_Email'
+      ))
+    ) {
+      await this.navigateToLoginForm()
     }
-    await this.navigateToLoginForm()
     const authenticated = await this.runInWorker('checkAuthenticated')
     if (!authenticated) {
       this.log('info', 'Not authenticated')
@@ -59,17 +87,17 @@ class TemplateContentScript extends ContentScript {
     if (!authenticated) {
       return true
     }
+    // await this.clickAndWait(logoutLinkSelector, loginLinkSelector)
     return true
   }
 
   async navigateToLoginForm() {
     this.log('info', '🤖 navigateToLoginForm')
-    await this.goto(baseUrl)
-  }
-
-  async checkAuthenticated() {
-    this.log('info', '🤖 checkAuthenticated')
-    return Boolean(document.querySelector())
+    await this.goto(loginFormUrl)
+    await Promise.race([
+      this.waitForElementInWorker('#CustomerLogin_CustomerLoginFormData_Email'),
+      this.waitForElementInWorker('.leftmenu__logout')
+    ])
   }
 
   async showLoginFormAndWaitForAuthentication() {
@@ -81,6 +109,11 @@ class TemplateContentScript extends ContentScript {
     await this.setWorkerState({ visible: false })
   }
 
+  async checkAuthenticated() {
+    this.log('info', '🤖 checkAuthenticated')
+    return Boolean(document.querySelector('.leftmenu__logout'))
+  }
+
   async getUserDataFromWebsite() {
     this.log('info', '🤖 getUserDataFromWebsite')
     return {
@@ -90,10 +123,19 @@ class TemplateContentScript extends ContentScript {
 
   async fetch(context) {
     this.log('info', '🤖 fetch')
+    if (this.store.userCredentials) {
+      this.log(
+        'info',
+        `this.store.userCredentials : ${JSON.stringify(
+          this.store.userCredentials
+        )}`
+      )
+      await this.saveCredentials(this.store.userCredentials)
+    }
   }
 }
 
-const connector = new TemplateContentScript()
-connector.init({ additionalExposedMethodsNames: [''] }).catch(err => {
+const connector = new CdiscountContentScript()
+connector.init({ additionalExposedMethodsNames: [] }).catch(err => {
   log.warn(err)
 })
